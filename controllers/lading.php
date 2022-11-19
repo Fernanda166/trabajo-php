b87c40f4faa477a55f4b7e23f286c6cf5cce42f8
Fernanda 166 <fluisa9@misena.edu.co>
2022-11-19 08:54:04 -0500

    subiendo esa monda

--- controllers/lading.php
+++ controllers/lading.php
@@ -2,7 +2,39 @@
     class Landing{
         public function __construct(){}
         public function index(){
-            echo "views/roles/business/header.php";
+            require_once "views/roles/business/header.php";
+            require_once "views/roles/business/index.view.php";
+            require_once "views/roles/business/footer.php";
+        }        
+        public function tables(){
+            require_once "views/roles/business/header.php";
+            require_once "views/business/tables.view.php";
+            require_once "views/roles/business/footer.php";
+        }
+        public function alerts(){
+            require_once "views/roles/business/header.php";
+            require_once "views/business/alerts.view.php";
+            require_once "views/roles/business/footer.php";
+        }
+        public function dataTables(){
+            require_once "views/roles/business/header.php";
+            require_once "views/business/dataTables.view.php";
+            require_once "views/roles/business/footer.php";
+        }
+        public function canvas(){
+            require_once "views/roles/business/header.php";
+            require_once "views/business/canvas.view.php";
+            require_once "views/roles/business/footer.php";
+        }
+        public function jsPdf(){
+            require_once "views/roles/business/header.php";
+            require_once "views/business/jsPdf.view.php";
+            require_once "views/roles/business/footer.php";
+        }
+        public function fullCalendar(){
+            require_once "views/roles/business/header.php";
+            require_once "views/business/fullCalendar.view.php";
+            require_once "views/roles/business/footer.php";
         }
     }
 ?>
