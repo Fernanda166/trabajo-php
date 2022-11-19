9d5602d98bc20e3a94bcf2a578d92d6822dece5d
Fernanda 166 <fluisa9@misena.edu.co>
2022-11-19 15:03:34 -0500

    subiendo esa monda

---
+++ controllers/Dashboard.php
@@ -0,0 +1,12 @@
+<?php
+    class Dashboard{
+        public function __construct(){}
+        public function index(){            
+            require_once "views/roles/admin/header.php";
+            require_once "views/roles/admin/admin_main.view.php";
+            require_once "views/roles/admin/footer.php";
+        }
+    }
+?>
+Footer
+© 2022 GitHub
